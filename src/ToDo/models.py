323f4d3aff66822ee8ce5c7a16b1f5f4a89b175b
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-27 14:09:09 +0200

    Task model

--- src/ToDo/models.py
+++ src/ToDo/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 User = get_user_model()
 
 class Task(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     action = models.CharField(max_length=700, verbose_name='Task')
     completed = models.BooleanField(default=False, verbose_name="Done")
     created_on = models.DateTimeField(auto_now_add=True)
