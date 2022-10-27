831b9ec1c5212e2c47a48e7abd4eb674664c270e
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-27 13:40:23 +0200

    CRUD views

--- src/ToDo/views.py
+++ src/ToDo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from models import Task
 
 # Create your views here.
+class TaskList(ListView):
+    model = Task
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ["task", "completed"]
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    template_name = "ToDo/task_edit.html"
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = reverse_lazy("ToDo:home")
