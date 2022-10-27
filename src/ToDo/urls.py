39882a547059039cd7bd7ee5634f62277f4aba77
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-27 14:05:58 +0200

    added CRUD urls

--- src/ToDo/urls.py
+++ src/ToDo/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from views import TaskList, TaskCreate, TaskUpdate, TaskDelete
 
 
 app_name = "ToDo"
 
 urlpatterns = [
-    path('', ),
+    path('<int:user_id>', TaskList.as_view(), name='home'),
+    path('create/<int:task_id>', TaskCreate.as_view(), name='create'),
+    path('edit/<int:task_id>', TaskDelete.as_view(), name='edit'),
+    path('delete/<int:task_id>', TaskDelete.as_view(), name='delete'),
 ]
