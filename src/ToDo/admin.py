1aa969b0de0cade6090c296f1d11ac2e95917e9c
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-29 00:47:33 +0200

    admin site

--- src/ToDo/admin.py
+++ src/ToDo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import Task
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["action", "completed"]
+    list_editable = ["action", "completed"]
+
+
+admin.site.register(Task, TaskAdmin)
