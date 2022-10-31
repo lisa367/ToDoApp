364f84075ce75f3ab023148d04fec52510d239ad
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-31 10:57:52 +0100

    sign up view

--- src/Website/views.py
+++ src/Website/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from .forms import SignUpForm
+
+
+User = get_user_model()
 
 
 class LandingPageView(TemplateView):
     template_name = "landing_page.html"
+
+
+class SignUpView(CreateView):
+    model = User
+    template_name = 'registration/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy("ToDo:home", )
