e12d554569f3ff9a28adb0d4a665b917634ebc69
lisa367 <75017410+lisa367@users.noreply.github.com>
2022-10-31 10:49:11 +0100

    CRUD urls

--- src/ToDo/urls.py
+++ src/ToDo/urls.py
@@ -6,8 +6,8 @@ from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete
 app_name = "ToDo"
 
 urlpatterns = [
-    path('<int:user_id>', TaskList.as_view(), name='home'),
+    path('<int:user_id>/', TaskList.as_view(), name='home'),
     path('create/<int:task_id>', TaskCreate.as_view(), name='create'),
-    path('edit/<int:task_id>', TaskDelete.as_view(), name='edit'),
+    path('edit/<int:task_id>', TaskUpdate.as_view(), name='edit'),
     path('delete/<int:task_id>', TaskDelete.as_view(), name='delete'),
 ]
